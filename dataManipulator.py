12057197cdea53589433020b54fb9b01cd0ffdf3
dajbassow <danielajbassow@gmail.com>
2022-01-27 10:49:50 +0100

    Edit stonks and stinks

--- dataManipulator.py
+++ dataManipulator.py
@@ -2,6 +2,9 @@ import sqlite3
 import math
 import random
 
+def get_tablename():
+    print('Enter your desired table: ')
+    return str(input())
 # add_data generates sales data for a year
 def add_data():
     try:
@@ -9,38 +12,40 @@ def add_data():
         db_connection = sqlite3.connect('Verkaufsdaten.db')
         cursor = db_connection.cursor()
         print("Connected to database!")
+        tablename = get_tablename()
         # Generate data for a year in a loop
         for i in range(1, 365):
             # Valentines day
-            if (i >= 40 & i<=45):
-                rand_int = random.randint(20, 40)
+            if (40 <= i <= 45):
+                rand_int = random.randint(100, 150)
                 sinus_result = rand_int * math.sin((i+80) * 8 * math.pi / 365) + 30
             # Black Friday
-            if (i >= 300 & i<=307):
-                rand_int = random.randint(35, 50)
+            if (300 <= i <= 307):
+                rand_int = random.randint(100, 150)
                 sinus_result = rand_int * math.sin((i+80) * 8 * math.pi / 365) + 30
             # Pre-Christmas-Time
-            if (i >= 335 & i<=356):
-                rand_int = random.randint(30, 45)
+            if (335 <= i <= 356):
+                rand_int = random.randint(100, 150)
                 sinus_result = rand_int * math.sin((i+80) * 8 * math.pi / 365) + 30
             # Christmas (Holidays, DROP)
-            if (i >= 357 & i<=361):
-                rand_int = round(random.uniform(0, 5), 2)
+            if (357 <= i <= 361):
+                rand_int = round(random.uniform(30, 20), 2)
+                print(rand_int)
                 sinus_result = rand_int * math.sin((i+80) * 8 * math.pi / 365) + 30
             # Every other day of  the year
             else:
                 sinus_result = 10 * math.sin((i+80) * 8 * math.pi / 365) + 30
             # Try to insert the result into the db
             try: 
-                table_name = str(input())
-                insert_query = f"INSERT INTO {table_name} (column1, column2) Values ({i}, {sinus_result})"
-                cursor.execute(insert_query)
+                #insert_query = f'INSERT INTO {tablename} (day, sales) Values ({i}, {round(sinus_result, 2)})'
+                update_statement = f'UPDATE {tablename} SET sales = {round(sinus_result, 2)} WHERE day = {i}'
+                cursor.execute(update_statement)
                 db_connection.commit()
             # Error handling
             except sqlite3.Error as error:
                 print('Error while inserting to your sqlite database.', error)
         # Select statement to see the results
-        select_query = 'SELECT * FROM Zahlen2021'
+        select_query = f'SELECT * FROM {tablename}'
         cursor.execute(select_query)
         record = cursor.fetchall()
         print("ma data: ", record[0])
