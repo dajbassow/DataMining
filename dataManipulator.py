4e404276e21920532d9f90d1c4525e9b80f402e6
dajbassow <70203189+dajbassow@users.noreply.github.com>
2022-03-25 16:02:51 +0100

    update project...

--- dataManipulator.py
+++ dataManipulator.py
@@ -2,6 +2,8 @@ import sqlite3
 import math
 import random
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 # Connect to db
 def get_db_connection():
@@ -14,6 +16,7 @@ def get_db_connection():
     except sqlite3.Error as error:
         print('Error while connecting to your sqlite database.', error)
 
+    return cursor, sqliteConnection
 
 # Close db connection
 def close_db_connection(cursor, db_connection):
@@ -33,7 +36,6 @@ def get_table_name():
 def generate_data(cursor, db_connection):
     print('Enter your desired years as an int: ')
     years = int(input())
-    new_years_sale_number = 0
     for i in range(0, years):
         table_name = get_table_name()
         rand_increas_number = round(random.uniform(0.1, 0.5), 2)
@@ -49,25 +51,25 @@ def generate_data(cursor, db_connection):
             # Valentines day
             if 38 <= i <= 42:
                 rand_num = random.randint(100, 120)
-                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + new_years_sale_number + 200
+                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 200
             # summer sale
             elif 182 <= i <= 235:
                 rand_num = random.randint(80, 85)
-                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + new_years_sale_number + 50
+                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
             # Black Friday
             elif 300 <= i <= 307:
                 rand_num = random.randint(100, 120)
-                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + new_years_sale_number + 50
+                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
             # christmas
             elif 335 <= i <= 356:
                 rand_num = random.randint(80, 90)
-                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + new_years_sale_number + 50
+                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
             # end of the year
             elif 357 <= i <= 365:
                 rand_num = round(random.uniform(0, 5), 2)
-                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + new_years_sale_number + 50          
+                sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50          
             else:
-                sinus_result = 0.1 * i + 50 * math.sin((i + 80) * 16 * math.pi / 365) + new_years_sale_number + 50
+                sinus_result = 0.1 * i + 50 * math.sin((i + 80) * 16 * math.pi / 365) + 50
             
             # Try to insert the result into the db
             try:
@@ -88,6 +90,8 @@ def fetch_data(cursor):
     select_query = f'SELECT * FROM {table_name}'
     cursor.execute(select_query)
     record = cursor.fetchall()
+    print(record)
+    print(record[1][1])
     return record
 
 def fetch_last_sale(cursor, table_name):
@@ -97,7 +101,7 @@ def fetch_last_sale(cursor, table_name):
     print(record)
     return record
 
-def draw_plot_data(cursor):
+def plot_single_data(cursor, sqliteConnection):
     table_name = get_table_name()
     sqlite_select_query = f'SELECT day FROM {table_name}'
     cursor.execute(sqlite_select_query)
@@ -109,8 +113,103 @@ def draw_plot_data(cursor):
     plt.plot(recordX, recordY)
     plt.show()
 
+# plot all 3 tables
+def plot_all_tables(cursor, sqliteConnection):
+    print("Plot data from sales 2019-2021...")
 
-cursor, db_connection = get_db_connection()
-generate_data(cursor, db_connection)
-draw_plot_data(cursor)
-close_db_connection(cursor, db_connection)
+    select_query = 'SELECT day FROM verkauf2019;'
+    cursor.execute(select_query)
+    sales19_x = cursor.fetchall()
+    select_query = 'SELECT sales FROM verkauf2019;'
+    cursor.execute(select_query)
+    sales19_y = cursor.fetchall()
+
+    select_query = 'SELECT day FROM verkauf2020;'
+    cursor.execute(select_query)
+    sales20_x = cursor.fetchall()
+    select_query = 'SELECT sales FROM verkauf2020;'
+    cursor.execute(select_query)
+    sales20_y = cursor.fetchall()
+
+    select_query = 'SELECT day FROM verkauf2021;'
+    cursor.execute(select_query)
+    sales21_x = cursor.fetchall()
+    select_query = 'SELECT sales FROM verkauf2021;'
+    cursor.execute(select_query)
+    sales21_y = cursor.fetchall()
+
+    plt.plot(sales19_x, sales19_y, label='sales2019')
+    plt.plot(sales20_x, sales20_y, label='sales2020')
+    plt.plot(sales21_x, sales21_y, label='sales2021')
+    plt.xlabel('day')
+    plt.ylabel('sales')
+    plt.title('Sales from 2019 - 2021')
+    plt.legend()
+    plt.show()
+
+# plots a sales table and their mean
+def plot_table_mean(cursor, sqliteConnection, table_name):
+    select_query = f'Select day from {table_name};'
+    cursor.execute(select_query)
+    sales_x = cursor.fetchall()
+    select_query = f'Select sales from {table_name};'
+    cursor.execute(select_query)
+    sales_y = cursor.fetchall()
+    sales_x_array = np.array(sales_x)
+    mean = get_mean(sqliteConnection, table_name)
+    std = get_standard_deviation(sqliteConnection, table_name)
+
+    plt.plot(sales_x, sales_y, label=f'{table_name}')
+    plt.plot(sales_x, sales_x_array * 0.1 + 75, label='linear gradient')
+    plt.axhline(mean, label='mean', color='r')
+    plt.axhline(std + mean, label='standard deviation', color='g')
+    plt.axhline(mean - std, color='g')
+    plt.xlabel('day')
+    plt.ylabel('sales')
+    plt.title('Sales from 2020 with mean and standard deviation')
+    plt.legend()
+    plt.show()
+
+# get mean from sales table
+def get_mean(sqliteConnection, table_name):
+    data = pd.read_sql(f"Select sales From {table_name}", con=sqliteConnection)
+    mean = data["sales"].mean()
+    print("mean: ", mean)
+    return mean
+
+# calculate standard deviation
+def get_standard_deviation (sqliteConnection, table_name):
+    data = pd.read_sql(f"Select sales From {table_name}", con=sqliteConnection)
+    st = data["sales"].std()
+    print("standard deviation: ", st)
+    return st
+
+# find sth unusual
+def get_peaks(sqlliteConnection, cursor):
+    print("looking for peaks in ... ")
+    table_name = get_table_name()
+    mean = get_mean(sqliteConnection, table_name)
+    std = get_standard_deviation(sqliteConnection, table_name)
+    sales_record = fetch_data(cursor, table_name)
+    print("mean + std: ", mean+std)
+    print("mean - std: ", mean-std)
+    upper_limit = mean + std
+    lower_limit = mean - std
+
+    for data in sales_record:
+        if data[1] > upper_limit or data[1] < lower_limit:
+            print("Peak found at day: ", data[0], " with a sale number of: ", data[1])
+
+
+cursor, sqliteConnection = get_db_connection()
+fetch_data(cursor, "Verkauf2020")
+# get_peaks(sqliteConnection, cursor)
+# generate_data(cursor, sqliteConnection)
+# table_name = get_table_name()
+# record = fetch_data(cursor, sqliteConnection, table_name)
+# plot_single_table(cursor, sqliteConnection)
+# plot_all_tables(cursor, sqliteConnection)
+# get_mean(sqliteConnection, 'verkauf2020')
+# get_standard_deviation(sqliteConnection, "verkauf2020")
+plot_table_mean(cursor, sqliteConnection, 'verkauf2020')
+close_db_connection(cursor, sqliteConnection)
